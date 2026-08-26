65be2263092d97d4aeabbe07b63e3534a385667e
Vitor Duarte <vd886032@gmail.com>
2026-08-26 10:02:39 -0300

    da continuidade proxima aula

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { teste } from "./teste";
+import { teste } from "./teste.js";
+import { exercicio1 } from "./poo/exercicio1.js";
 
 document.getElementById('btn')?.addEventListener('click', teste)
+document.getElementById('btn1')?.addEventListener('click', exercicio1)
--- src/poo/exercicio1.ts
+++ src/poo/exercicio1.ts
@@ -6,36 +6,38 @@
 //  Calcular Área,
 //  Calcular Perímetro.
  
+export function exercicio1(): void{
+    class Retangulo{
+        ladoA:number
+        ladoB:number
 
-class Retangulo{
-    ladoA:number
-    ladoB:number
+        constructor(A:number, B:number){
 
-    constructor(A:number, B:number){
+            this.ladoA = A
+            this.ladoB = B
+        }
 
-        this.ladoA = A
-        this.ladoB = B
+    mudarLado(novoLadoA:number, novoLadoB:number):void{
+            this.ladoA = novoLadoA
+            this.ladoB = novoLadoB
     }
+        retorna(ladoA:number, ladoB:number){
+            console.log(`O lado A è: ${ladoA}`)
+            console.log(`O lado B è: ${ladoB}`)
+        }
 
-   mudarLado(novoLadoA:number, novoLadoB:number):void{
-        this.ladoA = novoLadoA
-        this.ladoB = novoLadoB
-   }
-    retorna(ladoA:number, ladoB:number){
-        console.log(`O lado A è: ${ladoA}`)
-        console.log(`O lado B è: ${ladoB}`)
-    }
-
-    calcularArea():number {
-        return this.ladoA * this.ladoA;
-    }
-    calcularPerimentro():number {
-        return 2 * (this.ladoA + this.ladoB)
-    }
-    exibir():void{
+        calcularArea():number {
+            return this.ladoA * this.ladoA;
+        }
+        calcularPerimentro():number {
+            return 2 * (this.ladoA + this.ladoB)
+        }
+        exibir():void{
+            
+        }
         
-    }
-    
-}   
+    }   
 
-let novoRetangulo = new Retangulo(5, 8);
+    let novoRetangulo = new Retangulo(5, 8);
+
+}
